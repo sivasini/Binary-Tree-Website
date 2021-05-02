1e48976127117b066a1b8f0d4f36397ffca8f57b
Sivasini Netra <45381005+sivasini@users.noreply.github.com>
2021-05-02 19:36:10 +0530

    Image web scraping added

--- app.py
+++ app.py
@@ -1,5 +1,39 @@
 from flask import Flask, flash, redirect, render_template, request, session
 from datetime import timedelta
+import requests
+import urllib.request
+from bs4 import BeautifulSoup
+
+
+def scrap_images(image_url,filename):
+    opener = urllib.request.build_opener()
+    opener.addheaders = [('User-Agent','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36')]
+    urllib.request.install_opener(opener)
+    urllib.request.urlretrieve(image_url, filename)
+    print("Succefully extracted",filename)
+
+
+scrap_images("https://res.cloudinary.com/practicaldev/image/fetch/s--od-naD9n--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://miro.medium.com/max/975/1%2APWJiwTxRdQy8A_Y0hAv5Eg.png", 'static/bt1.png')
+scrap_images("https://static.javatpoint.com/ds/images/insertion-in-binary-search-tree.png", "static/insert.png")
+scrap_images("https://media.geeksforgeeks.org/wp-content/uploads/deletion-in-binary-tree.png", "static/delete.png")
+scrap_images("https://www.tutorialspoint.com/data_structures_algorithms/images/inorder_traversal.jpg", "static/inorder.jpg")
+scrap_images("https://www.tutorialspoint.com/data_structures_algorithms/images/preorder_traversal.jpg", "static/preorder.jpg")
+scrap_images("https://www.tutorialspoint.com/data_structures_algorithms/images/postorder_traversal.jpg", "static/postorder.jpg")
+scrap_images("https://cdn.programiz.com/sites/tutorial2program/files/tree_traversal_sub-tree-concept.png","static/tree.png")
+scrap_images("https://media.cheggcdn.com/media/cd3/cd3ff72a-2453-4ef6-b7f5-530696f32e32/phpVqes7i", "static/euler.png")
+scrap_images("https://i1.wp.com/kodebinary.com/wp-content/uploads/2019/04/image-87.png?w=809&ssl=1", "static/level.png")
+
+
+def scrap_properties():
+    def getdata(url):
+        r = requests.get(url)
+        return r.text
+    htmldata = getdata("https://www.geeksforgeeks.org/binary-tree-set-2-properties/")
+    soup = BeautifulSoup(htmldata, 'html.parser')
+    data = ''
+    for data in soup.find_all("p"):
+        print(data.get_text())
+
 
 app = Flask(__name__)
 app.config['REMEMBER_COOKIE_DURATION'] = timedelta(seconds=2000)
@@ -58,6 +92,46 @@ def types():
 @app.route('/depth')
 def depth():
     return render_template("depth.html", user=session['username'])
+
+
+@app.route('/v_insert')
+def v_insert():
+    return render_template("v_insert.html", user=session['username'])
+
+
+@app.route('/v_depth')
+def v_depth():
+    return render_template("v_depth.html", user=session['username'])
+
+
+@app.route('/v_height')
+def v_height():
+    return render_template("v_height.html", user=session['username'])
+
+
+@app.route('/in')
+def inorder():
+    return render_template("in.html", user=session['username'])
+
+
+@app.route('/pre')
+def pre():
+    return render_template("pre.html", user=session['username'])
+
+
+@app.route('/post')
+def post():
+    return render_template("post.html", user=session['username'])
+
+
+@app.route('/euler')
+def euler():
+    return render_template("euler.html", user=session['username'])
+
+
+@app.route('/level')
+def level():
+    return render_template("level.html", user=session['username'])
 
 
 @app.route('/logout')
